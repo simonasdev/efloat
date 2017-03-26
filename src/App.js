ad0704edfc5420ed58cd6357626ce1aa40eb5b18
simonasdev <simonasgildutis@gmail.com>
2017-03-26 22:12:52 +0300

    works

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react';
-import { StyleSheet, ToastAndroid, Alert } from 'react-native';
-import MapView from 'react-native-maps';
-import BluetoothSerial from 'react-native-bluetooth-serial';
+import React, { Component } from 'react'
+import { StyleSheet, ToastAndroid, Alert } from 'react-native'
+import MapView from 'react-native-maps'
+import BluetoothSerial from 'react-native-bluetooth-serial'
+// import reactMixin from 'react-mixin'
+// import TimerMixin from 'react-timer-mixin';
 
 export default class efloat extends Component {
   constructor (props) {
     super(props)
+
+    const initialCoords = {
+      latitude: 54.6872,
+      longitude: 25.2797,
+    }
+
     this.state = {
       isEnabled: false,
       connected: false,
       device: null,
+
       caught: false,
-      latitude: 54.6872,
-      longitude: 25.2797,
       lights: false,
+
+      marker: null,
+      region: {
+        ...initialCoords,
+        ...this.deltas(initialCoords)
+      }
     }
   }
 
@@ -45,26 +58,30 @@ export default class efloat extends Component {
   }
 
   render() {
-    const { latitude, longitude } = this.state
+    const { latitude, longitude, marker, region } = this.state
 
     return (
       <MapView
         style={{flex: 1}}
-        region={{
-          latitude: latitude,
-          longitude: longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+        initialRegion={region}
       >
-        <MapView.Marker
-          coordinate={{ latitude: latitude, longitude: longitude }}
-          title={'Pludė'}
-          description={'Spausk junginėt lempas'}
-          onCalloutPress={this.toggleLights}
-        />
+        { marker &&
+            <MapView.Marker
+              coordinate={marker}
+              title={'Pludė'}
+              description={'Spausk junginėt lempas'}
+              onCalloutPress={this.toggleLights}
+            />
+        }
       </MapView>
     );
+  }
+
+  deltas ({ latitude, longitude }) {
+    return {
+      latitudeDelta: 0.0122,
+      longitudeDelta: 0.0121,
+    }
   }
 
   alert (message) {
@@ -81,7 +98,7 @@ export default class efloat extends Component {
         this.alert(`Connected to ${device.name}`)
         this.setState({ device, connected: true })
 
-        BluetoothSerial.subscribe("\n")
+        BluetoothSerial.subscribe("a*6f")
       })
       .catch((err) => {
         this.alert(err.message)
@@ -94,29 +111,47 @@ export default class efloat extends Component {
   }
 
   parseMessage (data) {
-    console.log(data)
-    if (data.indexOf('$STATE') > -1) {
-      const caught = data.split(',')[1][0] === '1'
+    const [caughtState, coordinates] = data.trim().split("\n")
+    let newState = {}
+
+    if (caughtState) {
+      const caught = caughtState.split(',')[1][0] === '1'
 
       if (caught !== this.state.caught) {
-        this.setState({ caught })
+        newState.caught = caught
 
-        if (caught) {
-          Alert.alert(
+        if (caught && !this.activeAlert) {
+          this.activeAlert = Alert.alert(
             'Fish caught!',
             'Go fetch!',
-            [{ text: 'Got it!', onPress: () => this.write('R') }],
+            [{
+              text: 'Got it!',
+              onPress: () => {
+                this.activeAlert = null;
+                this.write('R')
+              }
+            }],
             { cancelable: false }
           )
         }
       }
-    } else {
-      const components = data.split(',')
+    }
+
+    if (coordinates) {
+      const components = coordinates.split(',')
       const latitude = parseFloat(components[1]) / 100,
             longitude = parseFloat(components[3]) / 100;
 
-      this.setState({ latitude, longitude })
+      let markerState = {}
+      const latLng = { latitude, longitude }
+
+      Object.assign(markerState, { marker: latLng })
+      if (!this.state.marker) Object.assign(markerState, { region: Object.assign(latLng, this.deltas(latLng)) })
+
+      Object.assign(newState, markerState)
     }
+
+    this.setState(newState)
   }
 
   toggleLights = () => {
@@ -148,3 +183,5 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+// reactMixin(efloat.prototype, TimerMixin);
